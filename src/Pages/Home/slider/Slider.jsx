757c5fd87dda37f25ed1dec281f8295f4659e79b
rubayet-hasan-yasin <rubayethasanyasin@gmail.com>
2023-07-25 18:52:27 +0600

    slider image fix

--- src/Pages/Home/slider/Slider.jsx
+++ src/Pages/Home/slider/Slider.jsx
@@ -36,42 +36,42 @@ const Slider = () => {
 
 
     return (
-        <div className="flex justify-center">
+        <div className="flex justify-center mt-40">
             <div className="scene">
                 <div className="carousel keen-slider" ref={sliderRef}>
-                    <div className="carousel__cell">
-                        <img src="https://hotwheelsunleashed.com/wp-content/uploads/2022/04/Veicolo_HOT-WHEELS%E2%84%A2-ScreenshotCollection_084_Camera_Marketing.png" alt="" className="w-full h-full object-cover"/>
-                        <p className="text-center text-xs font-extrabold">BMW M3 GT2</p>
+                    <div className="carousel__cell flex flex-col">
+                        <img src="https://i.ibb.co/41ztjZJ/Hot-Wheels-PNG-Transparent-Picture.png" alt="" className="w-full"/>
+                        <p className="text-center text-xs font-extrabold mt-auto">Twinduction 176/250 MISB</p>
                     </div>
 
 
-                    <div className="carousel__cell ">
-                    <img src="https://hotwheelsunleashed.com/wp-content/uploads/2022/04/Veicolo_HOT-WHEELS%E2%84%A2-ScreenshotCollection_160_Camera_Marketing.png" alt="" className="w-full h-full object-cover"/>
-                    <p className="text-center text-xs font-extrabold">RIVITED</p>
+                    <div className="carousel__cell flex flex-col">
+                    <img src="https://i.ibb.co/Kj3NQgM/Hot-Wheels-PNG-Image.png" alt="" className="w-full"/>
+                    <p className="text-center text-xs font-extrabold mt-auto">Chevrolet Corvette Z06</p>
                     </div>
 
 
-                    <div className="carousel__cell ">
-                    <img src="https://hotwheelsunleashed.com/wp-content/uploads/2022/04/Veicolo_HOT-WHEELS%E2%84%A2-ScreenshotCollection_096_Camera_Marketing.png" alt="" className="w-full h-full object-cover"/>
-                    <p className="text-center text-xs font-extrabold">McLaren 720S</p>
+                    <div className="carousel__cell flex flex-col">
+                    <img src="https://i.ibb.co/qFrfzW4/customized-vehicle-1536x876.png" alt="" className="w-full"/>
+                    <p className="text-center text-xs font-extrabold mt-auto">Twin Mill</p>
                     </div>
 
 
-                    <div className="carousel__cell ">
-                    <img src="https://hotwheelsunleashed.com/wp-content/uploads/2022/04/Veicolo_HOT-WHEELS%E2%84%A2-ScreenshotCollection_098Camera_Marketing-1.png" alt="" className="w-full h-full object-cover"/>
-                    <p className="text-center text-xs font-extrabold">Aston Martin DBS</p>
+                    <div className="carousel__cell flex flex-col">
+                    <img src="https://i.ibb.co/tHDv3zM/Hot-Wheels-PNG-File.png" alt="" className="w-full"/>
+                    <p className="text-center text-xs font-extrabold mt-auto">Bone Shaker</p>
                     </div>
 
 
-                    <div className="carousel__cell ">
-                    <img src="https://hotwheelsunleashed.com/wp-content/uploads/2022/03/Veicolo_HOT-WHEELS%E2%84%A2-ScreenshotCollection_41.png" alt="" className="w-full h-full object-cover"/>
-                    <p className="text-center text-xs font-extrabold">Mad Manga</p>
+                    <div className="carousel__cell flex flex-col">
+                    <img src="https://i.ibb.co/sQ8j2GK/kisspng-ferrari-599xx-car-ferrari-testarossa-chevrolet-cor-hot-wheels-5ac34b24c7f333-117234651522748.png" alt="" className="w-full"/>
+                    <p className="text-center text-xs font-extrabold mt-auto">Ferrari 599XX</p>
                     </div>
 
 
-                    <div className="carousel__cell ">
-                    <img src="https://hotwheelsunleashed.com/wp-content/uploads/2022/02/Veicolo_HOT-WHEELS%CE%93ao-ScreenshotCollection_095_Camera_Marketing.png" alt="" className="w-full h-full object-cover"/>
-                    <p className="text-center text-xs font-extrabold">McLaren Senna</p>
+                    <div className="carousel__cell flex flex-col">
+                    <img src="https://i.ibb.co/MffBmqY/kisspng-car-lamborghini-aventador-hot-wheels-die-cast-toy-lamborghini-5abc31a813e513-117504381522282.png" alt="" className="w-full"/>
+                    <p className="text-center text-xs font-extrabold mt-auto">Lamborghini Veneno</p>
                     </div>
                     
                 </div>
